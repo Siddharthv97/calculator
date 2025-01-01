d0179215e7775db1b79a5e4839664a073cb9bbdf
Siddharth Vishwakarma <Vissid05@gmail.com>
2025-01-01 19:39:07 +0530

    Calculator v1

---
+++ script.js
@@ -0,0 +1,9 @@
+let display = document.querySelector("#display");
+let btn = document.querySelectorAll(".btn")
+
+console.log(btn);
+
+function displayInput(number){
+    number == document.querySelector("#display").innerText;
+    console.log(number);
+}
