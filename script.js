b6789265361e734b01311b0919d81da7aa69dcd1
Siddharth Vishwakarma <Vissid05@gmail.com>
2025-01-02 17:02:42 +0530

    Project was completed

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 let display = document.querySelector("#display");
-let btn = document.querySelectorAll(".btn")
-
-console.log(btn);
-
-function displayInput(number){
-    number == document.querySelector("#display").innerText;
-    console.log(number);
+let btn = document.querySelectorAll(".btn");
+let br = document.querySelector("#bracket");
+function clearDisplay(){
+    display.value = "";
 }
+
+function displayInput(number) {
+    display.value += number;
+}
+
+function equal(){
+    let result = display.value;
+    display.value = eval(result);
+}
+
+function del(){
+    display.value = display.value.slice(0, -1);
+}
+
+function bracket(){
+    br.addEventListener("click", (e)=>{
+        e.target.displayInput = '('
+        console.log("Clicked")
+    })
+}
+// else{
+//     console.log("else was clicked")
+// }
